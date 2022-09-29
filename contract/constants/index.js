95414655f8f27807da58fb3faa982dceda51836b
meharab <meharablatif@hotmail.com>
2022-09-29 09:13:30 +0600

    added contracts script and config files

---
+++ contract/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x95E37c97A0ba851530357F41a53f5EA274605793";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
